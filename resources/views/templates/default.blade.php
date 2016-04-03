eb08a3578ba8d343cf20208bc79cf41bfd5edfeb
Mgitau <mbugit88@gmail.com>
2016-04-03 14:39:19 +0300

    changed Application title

--- resources/views/templates/default.blade.php
+++ resources/views/templates/default.blade.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>H-Desk</title>
+	<title>H-Deskk</title>
 	<meta name="viewport" content="width=device-width, initail-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.10.0/css/bootstrap-select.min.css">
 	<link rel="stylesheet" type="text/css" href="css/hdesk.css">
 <!--    <link rel="stylesheet" href="{{ asset('hdesk.css') }}">-->
-   
+
 
 </head>
 <body>
