9a3972d42af44baab197b175a935c159f7f52c74
Mgitau <mbugit88@gmail.com>
2016-07-12 16:29:30 +0300

    Working on soft delete

--- app/Http/Controllers/TrashController.php
+++ app/Http/Controllers/TrashController.php
@@ -9,25 +9,24 @@ use Hdesk\Http\Requests;
 class TrashController extends Controller
 {
     public function getTrashbin(){
-<<<<<<< HEAD
-      // $tickets = \Hdesk\Models\Ticket::onlyTrashed()->orderBy('id', 'desc')->paginate(10);
-      $tickets = \Hdesk\Models\Ticket::withTrashed()->get();
-    // dd($tickets);
-=======
-      $tickets = \Hdesk\Models\Ticket::onlyTrashed()->orderBy('id', 'desc')->paginate(15);
->>>>>>> 383e46e4a46d810ef386a578c061a044554de2c2
+
+      $tickets = Ticket::onlyTrashed()->where('deleted_at');
+    //  $tickets = \Hdesk\Models\Ticket::withTrashed()->get();
+      //$tickets = \Hdesk\Models\Ticket::onlyTrashed()->orderBy('id', 'desc')->paginate(15);
       return view('trash.trashbin')->with('tickets', $tickets);
+
     }
 
     public function getTrashTicket($id){
-     $ticket = \Hdesk\Models\Ticket::find($id)->delete();
-     return redirect()->route('dashboard.index')->with('info', 'Ticket has been Trashed');
+     $ticket = Ticket::find($id);
+     $ticket->delete();
+     return redirect()->route('tickets.open')->with('info', 'Ticket has been Trashed');
 
     }
 
-    public function getTrashbinRestore($id){
-      $ticket = \Hdesk\Models\Ticket::onlyTrashed()->where('id',$id)->restore();
-      return redirect()->back()->with('info', 'Ticket Restored');
-      //$this->getTrashBin();
-    }
-}
+//     public function getTrashbinRestore($id){
+//       $ticket = Ticket::onlyTrashed()->where('id',$id)->restore();
+//       return redirect()->back()->with('info', 'Ticket Restored');
+//       //$this->getTrashBin();
+//     }
+ }
