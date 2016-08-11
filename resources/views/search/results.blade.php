f78f5fd8f0873cb04499efd4d6be2e5a3364cfdc
Mbugua Gitau <mbugua.gitau@triad.co.ke>
2016-08-11 15:53:22 +0300

    fixed ticket view layout

--- resources/views/search/results.blade.php
+++ resources/views/search/results.blade.php
@@ -5,197 +5,102 @@
 
 <div class="container">
 
-    <div class="hdesk-container">
 
-        @if(!$ticket)
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h4 class="text-center">{{$ticket->subject}}</h4>
+        </div><!-- panel-heading-->
 
-          <div class="alert alert-danger">
-		          <p>No ticket found, sorry</p>
-	       </div>
+        <div class="panel-body">
+          @if(!$ticket)
 
-        @else
+            <div class="alert alert-danger">
+                <p>No ticket found, sorry</p>
+           </div>
 
-        <div class="row">
-          <div class="col-md-12">
-            <h4 class="text-center">{{$ticket->subject}}</h4>
-          </div>
+          @else
 
-          @if(Auth::check())
-          <span class="pull-right">
-            @if($ticket->status === 'Closed')
-              <a class="btn btn-primary" href="{{ Route('ticket.edit', ['ticket_id' => $ticket->id]) }}" role="button"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span> Edit</a>
-              <a class="btn btn-danger" href="{{ Route('ticket.trash', ['ticket_id' => $ticket->id]) }}" role="button"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span> Trash</a>
-            @else
-              <a class="btn btn-success" href="{{ Route('status.closed', ['ticket_id' => $ticket->id]) }}" role="button"><span class="glyphicon glyphicon-check" aria-hidden="true"></span> Mark Closed</a>
-              <a class="btn btn-primary" href="{{ Route('ticket.edit', ['ticket_id' => $ticket->id]) }}" role="button"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span> Edit</a>
-              <a class="btn btn-danger" href="{{ Route('ticket.trash', ['ticket_id' => $ticket->id]) }}" role="button"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span> Trash</a>
+            @if(Auth::check())
+            <span class="pull-right">
+              @if($ticket->status === 'Closed')
+                <a class="btn btn-primary" href="{{ Route('ticket.edit', ['ticket_id' => $ticket->id]) }}" role="button"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span> Edit</a>
+                <a class="btn btn-danger" href="{{ Route('ticket.trash', ['ticket_id' => $ticket->id]) }}" role="button"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span> Trash</a>
+              @else
+                <a class="btn btn-success" href="{{ Route('status.closed', ['ticket_id' => $ticket->id]) }}" role="button"><span class="glyphicon glyphicon-check" aria-hidden="true"></span> Mark Closed</a>
+                <a class="btn btn-primary" href="{{ Route('ticket.edit', ['ticket_id' => $ticket->id]) }}" role="button"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span> Edit</a>
+                <a class="btn btn-danger" href="{{ Route('ticket.trash', ['ticket_id' => $ticket->id]) }}" role="button"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span> Trash</a>
+              @endif
+            </span>
             @endif
-          </span>
-          @endif
 
-          </div>
-          <div class="row">
-            <div class="col-md-2">
+            <table class="table">
+              <tr>
+                <td class="no-border"><b>Ticket ID:</b></td>
+                <td class="no-border">{{$ticket->ticket_no}}</td>
+              </tr>
+
+              <tr>
+                <td class="no-border"><b>Ticket Status:</b></td>
+                <td class="no-border">
+                   @if($ticket->status === 'Open')
+                    <span class="statusopen"> {{$ticket->status}}</span>
+
+                    @elseif($ticket->status === 'Pending')
+                      <span class="statuspending"> {{$ticket->status}}</span>
+
+                    @elseif($ticket->status === 'Closed')
+                      <span class="statusclosed"> {{$ticket->status}}</span>
+
+                   @endif
+                </td>
+              </tr>
+
+              <tr>
+                <td class="no-border"><b>Category</b></td>
+                <td class="no-border">{{$ticket->category}}</td>
+              </tr>
+
+              <tr>
+                <td class="no-border"><b>Priority:</b></td>
+                <td class="no-border">{{$ticket->priority}}</td>
+              </tr>
+
+              <tr>
+                <td class="no-border"><b>Created on:</b></td>
+                <td class="no-border">{{$ticket->created_at->diffForHumans()}}</td>
+              </tr>
+
+              <tr>
+                <td class="no-border"><b>Updated on:</b></td>
+                <td class="no-border">{{$ticket->updated_at->diffForHumans()}}</td>
+              </tr>
+
+              <tr>
+                <td class="no-border"><b>Name:</b></td>
+                <td class="no-border">{{$ticket->name}}</td>
+              </tr>
+
+              <tr>
+                <td class="no-border"><b>Email:</b></td>
+                <td class="no-border">{{$ticket->email}}</td>
+              </tr>
+
+              <tr>
+                <td class="no-border"><b>Subject:</b></td>
+                <td class="no-border">{{$ticket->subject}}</td>
+              </tr>
+
+              <tr>
+                <td class="no-border"><b>Message:</b></td>
+                <td class="no-border">{{$ticket->message}}</td>
+              </tr>
+
+            </table><!--table-->
+
+        </div><!--panel-body-->
+
+      </div>
 
-               <p><b>Ticket ID:</b></p>
-
-            </div> <!--end col-md-2-->
-
-            <div class="col-md-10">
-
-               {{$ticket->ticket_no}}
-
-            </div> <!--end col-md-10-->
-
-        </div> <!--end row-->
-
-        <div class="row">
-
-            <div class="col-md-2">
-
-               <p><b>Ticket Status:</b></p>
-
-            </div> <!--end col-md-2-->
-
-            <div class="col-md-10">
-
-              {{$ticket->status}}
-
-            </div> <!--end col-md-10-->
-
-        </div> <!--end row-->
-
-        <div class="row">
-
-            <div class="col-md-2">
-
-               <p><b>Category:</b></p>
-
-            </div> <!--end col-md-2-->
-
-            <div class="col-md-10">
-
-                {{$ticket->category}}
-
-            </div>
-
-        </div> <!--end row-->
-
-        <div class="row">
-
-            <div class="col-md-2">
-
-               <p><b>Priority:</b></p>
-
-            </div> <!--end col-md-2-->
-
-            <div class="col-md-10">
-
-                {{$ticket->priority}}
-
-            </div>
-
-        </div> <!--end row-->
-
-        <div class="row">
-
-            <div class="col-md-2">
-
-               <p><b>Created on:</b></p>
-
-            </div> <!--end col-md-2-->
-
-            <div class="col-md-10">
-
-                {{$ticket->created_at->diffForHumans()}}
-
-            </div>
-
-        </div> <!--end row-->
-
-        <div class="row">
-
-            <div class="col-md-2">
-
-               <p><b>Updated on:</b></p>
-
-            </div> <!--end col-md-2-->
-
-            <div class="col-md-10">
-
-                {{$ticket->updated_at->diffForHumans()}}
-
-            </div>
-
-        </div> <!--end row-->
-
-        <hr class="hdesk-hr">
-
-        <div class="row">
-
-            <div class="col-md-2">
-
-               <p><b>Name:</b></p>
-
-            </div> <!--end col-md-2-->
-
-            <div class="col-md-10">
-
-                {{$ticket->name}}
-
-            </div>
-
-        </div> <!--end row-->
-
-        <div class="row">
-
-            <div class="col-md-2">
-
-               <p><b>Email:</b></p>
-
-            </div> <!--end col-md-2-->
-
-            <div class="col-md-10">
-
-                {{$ticket->email}}
-
-            </div>
-
-        </div> <!--end row-->
-
-        <div class="row">
-
-            <div class="col-md-2">
-
-               <p><b>Subject:</b></p>
-
-            </div> <!--end col-md-2-->
-
-            <div class="col-md-10">
-
-               {{$ticket->subject}}
-
-            </div>
-
-        </div> <!--end row-->
-
-        <div class="row">
-
-            <div class="col-md-2">
-
-               <p><b>Message:</b></p>
-
-            </div> <!--end col-md-2-->
-
-            <div class="col-md-10">
-
-               {{$ticket->message}}
-
-            </div>
-
-        </div> <!--end row-->
-
-    </div> <!--end hdesk-container-->
     <br>
   @foreach ($ticket->comments as $comment)
 
@@ -234,6 +139,7 @@
           @endif
         </div>
         </div>
+
       @elseif($comment->corrective_action)
 
       <div class="panel panel-danger">
@@ -255,6 +161,8 @@
     @endif
     @endforeach
 
+
+<!-- Popup Modals -->
     @if(Auth::check())
       <div class="hdesk-container">
         <div class="row">
